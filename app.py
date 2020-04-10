7002d449b745f72f40806dcfd902b9b5b6127cd4
Vasyl Herman <vasil.german@gmail.com>
2020-04-10 18:05:03 +0300

    init

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import create_engine
-from sqlalchemy import Table, Column, Integer, String, MetaData, ForeignKey
+# from sqlalchemy import Table, Column, Integer, String, MetaData, ForeignKey
 
 app = Flask(__name__)
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
