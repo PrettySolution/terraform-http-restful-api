f80db5a8cc85c46bdda0d5f6175305c8bf63e133
Vasyl Herman <vasil.german@gmail.com>
2020-04-10 18:10:18 +0300

    init

--- app.py
+++ app.py
@@ -1,20 +1,21 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import create_engine
-# from sqlalchemy import Table, Column, Integer, String, MetaData, ForeignKey
+from sqlalchemy import Table, Column, Integer, String, MetaData, ForeignKey
 
 app = Flask(__name__)
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
-# db = SQLAlchemy(app)
-#
-# engine = create_engine('sqlite:///database.db', echo=False)
-# metadata = MetaData()
-# historical_data = Table('historical_data', metadata,
-#                         Column('id', Integer, primary_key=True),
-#                         Column('reqId', String),
-#                         Column('bar_date', String)
-#                         )
-# metadata.create_all(engine)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+engine = create_engine('sqlite:///database.db', echo=False)
+metadata = MetaData()
+historical_data = Table('historical_data', metadata,
+                        Column('id', Integer, primary_key=True),
+                        Column('reqId', String),
+                        Column('bar_date', String)
+                        )
+metadata.create_all(engine)
 
 
 @app.route('/')
